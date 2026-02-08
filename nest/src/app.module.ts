e31977dc2c37916e358a3468596b1e6d75f2f095
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2026-02-07 22:29:02 -0300

    feat: add @nestjs/event-emitter and integrate it into the AppModule

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { MoviesModule } from './movies/movies.module';
 import { DatabaseModule } from './infra/mongo/database.module';
 import { CacheModule } from './infra/cache/cache.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [DatabaseModule, CacheModule, MoviesModule],
+  imports: [
+    EventEmitterModule.forRoot(),
+    DatabaseModule,
+    CacheModule,
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
